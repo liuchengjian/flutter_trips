431d36f9e6a2336dfc1629dfe1f95002e55c57bd
lchj <627107345@qq.com>
2019-05-27 15:09:20 +0800

    Initial commit

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("首页"),
+      ),
+    );
+  }
+}
